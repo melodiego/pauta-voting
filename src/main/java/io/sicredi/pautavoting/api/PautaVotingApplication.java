cc0caee7589aefb6329348d38d2312d93ab06b99
Diego Melo <melo_diego@sicredi.com.br>
2020-12-23 09:16:46 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/io/sicredi/pautavoting/api/PautaVotingApplication.java
@@ -0,0 +1,13 @@
+package io.sicredi.pautavoting.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PautaVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PautaVotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/sicredi/pautavoting/api/PautavotingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sicredi.pautavoting.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PautavotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
